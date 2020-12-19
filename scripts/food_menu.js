d95d7f0411f480b45545707d7bff171bfb8933a1
EoinJSD <eoinjsd@outlook.com>
2020-12-19 12:20:24 +0000

    Adding new Food Menu features and Styles
    Food Menu - Js for filters

---
+++ scripts/food_menu.js
@@ -0,0 +1,10 @@
+// Add active class to the current button (highlight it)
+var filterOptions = document.getElementById("filterOptions");
+var filter = filterOptions.getElementsByClassName("filter");
+for (var i = 0; i < filter.length; i++) {
+  filter[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
